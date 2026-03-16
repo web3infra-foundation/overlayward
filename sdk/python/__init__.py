ee9ccfd7ce0e6549d81057a9cd8613c72da26b1a
YingkaiLi-VM <c.asm.lyk@gmail.com>
2026-03-16 14:14:04 +0800

    Delete sdk directory
    Signed-off-by: YingkaiLi-VM <c.asm.lyk@gmail.com>

--- sdk/python/__init__.py
+++
@@ -1,9 +0,0 @@
-# Overlayward Python SDK
-# Auto-generated gRPC stubs + Pythonic wrapper
-#
-# Usage:
-#   from overlayward import Client
-#   client = Client(endpoint="localhost:8421", token="ow-agent-token")
-#   sandbox = client.sandbox.create(name="dev", cpu=4, memory="8GB")
-#
-# Generate stubs:  make gen-python
